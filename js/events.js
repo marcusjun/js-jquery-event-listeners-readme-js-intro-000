8863f2edd4917e072ae4b0afb04efa15db71ba35
Marcus Jun <mjun@calstate.edu>
2018-03-17 02:48:54 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,41 @@
 //define functions here
 
+function getIt(){
+  $('p').on('click',function(){
+    alert("Hey!")
+  })
+}
+
+function frameIt(){
+  $('img').on('load',function(){
+    //There is a jquery function to addClass(classname)
+    //The classname should not start with . period
+    //https://api.jquery.com/addClass/
+    $('img').addClass('tasty')
+  })
+}
+
+function pressIt(){
+  $('input').on('keydown',function(key){
+    if (key.which===71){
+      alert("G was pressed")
+    }
+  })
+}
+
+function submitIt(){
+  $('form').on('submit',function(){
+    alert("Your form is going to be submitted now.")
+  })
+}
+
 $(document).ready(function(){
 
 // call functions here
-
+/*
+  getIt()
+  frameIt()
+  pressIt()
+  submitIt()
+*/
 });
